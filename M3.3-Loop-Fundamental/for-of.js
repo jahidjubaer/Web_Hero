cbedb92d1602bcc253db6b59dfcaa80c65e30456
Jahid Hasan <jahidjubaer17@gmail.com>
2025-07-29 21:18:18 +0600

    loop added

---
+++ M3.3-Loop-Fundamental/for-of.js
@@ -0,0 +1,18 @@
+// loop - array number 
+let number = [23, 43, 43, 12, 42, 534];
+console.log(number);
+
+// output using loop 
+for(let num of number){
+    console.log(num);
+}
+
+// loop - string 
+let food = ['fish', 'beef', 'rice', 'mango', 'milk'];
+console.log(food);
+
+//output using loop
+for(let item of food){
+    console.log(item);
+    console.log('i wanna eat!');
+}
