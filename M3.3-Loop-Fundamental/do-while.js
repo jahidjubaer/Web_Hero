b014251938af2a0d457ea3441fdd4b222420451c
Jahid Hasan <jahidjubaer17@gmail.com>
2025-07-30 16:14:19 +0600

    do while loop added

---
+++ M3.3-Loop-Fundamental/do-while.js
@@ -0,0 +1,13 @@
+// normal while loop
+let n = 1;
+while(n <= 10){
+    console.log(n);
+    n++;
+}
+
+// do-while loop - minimum 1 time run this code ;
+let num = 0;
+do{
+    console.log(num);
+    num++;
+}while(num <= 10);
