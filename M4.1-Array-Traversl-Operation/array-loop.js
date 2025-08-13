fcebbe26bd5f5aa1ae54a49a4ff5518264a444df
Jahid Hasan <jahidjubaer17@gmail.com>
2025-08-13 20:12:40 +0600

    array-loop

---
+++ M4.1-Array-Traversl-Operation/array-loop.js
@@ -0,0 +1,28 @@
+let fruits = ['mango', 'apple', 'banana', 'daav', 'taal'];
+
+// traverse using for loop 
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
+
+let number = [4, 4, 6, 2, 5, 9, 2];
+
+// traverse number array using for loop 
+for(let i = 0; i < number.length; i++){
+    console.log(number[i]);
+}
+
+//traverse using while
+let n = 0;
+while(fruits.length > n){
+    console.log(fruits[n]);
+    n++;
+}
+
+//traverse using while
+let z = 0;
+while(number.length > z){
+    console.log(number[z]);
+    z++;
+}
+
