98e184cc187488f5d3747dd595a988f09f3090e5
Jahid Hasan <jahidjubaer17@gmail.com>
2025-08-14 00:05:36 +0600

    2D array

---
+++ M4.1-Array-Traversl-Operation/2D-array.js
@@ -0,0 +1,26 @@
+let marks = [
+    [4, 3, 5, 2, 64, 2],
+    [4, 2, 6, 2, 6, 49],
+    [3, 6, 2, 5, 4]
+]
+
+console.log(marks); // out the full array 
+// if we want to print first array 
+console.log(marks[0]);
+
+// to access first array 3 element
+console.log(marks[0][2]);
+
+// you can push pop and other 
+marks[0].push(48);
+marks[1].pop();
+console.log(marks);
+
+// loop 
+for(index of marks){
+    console.log(index[0]);
+    
+}
+
+
+
