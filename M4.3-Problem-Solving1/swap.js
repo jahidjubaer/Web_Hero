794fc4e01e4bb253b21f34afe7b8ca208ceee2cb
Jahid Hasan <jahidjubaer17@gmail.com>
2025-08-15 18:44:46 +0600

    swap

---
+++ M4.3-Problem-Solving1/swap.js
@@ -0,0 +1,15 @@
+// lets see how to swap the value of variable 
+
+let a = 34, b = 39;
+
+// using temp variable 
+let temp = a;
+a = b;
+b = temp;
+console.log(a, b);
+
+// i don't know whats the name of method but you can swap like this
+[a, b] = [b, a];
+console.log(a, b);
+
+
