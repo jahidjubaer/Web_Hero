1656abd5c0c8b32250cd5ac46d57352ba8e0ed0d
Jahid Hasan <jahidjubaer17@gmail.com>
2025-08-25 17:13:57 +0600

    dom started

---
+++ MileStone5-InteractiveJs-DOM/M5.2-DOM/js/first.js
@@ -0,0 +1,8 @@
+console.log("This is form outside");
+
+// we can print or show document (HTML) from here using object 
+console.log(document);
+// we can also access element , arttribue and other thing form document ; like  
+console.log(document.body);
+
+
