c751b263f75acac58ec0eafdee3ad9000476ee49
Jahid Hasan <jahidjubaer17@gmail.com>
2025-09-15 09:32:04 +0600

    entries

---
+++ MileStone-6-Intermediate-JavaScript-API/M6.1-JS-Recap-and-Basic-ES6/entries.js
@@ -0,0 +1,17 @@
+// key, value, entires ;
+
+const employee = {
+    name : 'jahid hasan',
+    salary : 1500,
+    designation : 'software engineer ',
+    experience : 1
+}
+
+// i can find only the value ;
+console.log(Object.values(employee));
+// keys
+console.log(Object.keys(employee));
+// entries
+console.log(Object.entries(employee));
+
+
