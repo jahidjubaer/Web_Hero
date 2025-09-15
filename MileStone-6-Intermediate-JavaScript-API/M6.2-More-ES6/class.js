2556c619f6f87f1b0d14ac13405f575a17636900
Jahid Hasan <jahidjubaer17@gmail.com>
2025-09-16 01:32:14 +0600

    class

--- MileStone-6-Intermediate-JavaScript-API/M6.2-More-ES6/class.js
+++ MileStone-6-Intermediate-JavaScript-API/M6.2-More-ES6/class.js
@@ -1,33 +1,31 @@
-// class 
+// class
 
-// this is how write a class -> class is like an template ; 
-// where have properties and method ; 
+// this is how write a class -> class is like an template ;
+// where have properties and method ;
 class player {
+  // constructor method ; where we write properties ;
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.location = "Bangladesh";
+  }
 
-    // constructor method ; where we write properties ; 
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-        this.location = 'Bangladesh';
-    }
+  // goal method -> like an function , work for only player class;
+  goal() {
+    console.log("scored a goal");
+  }
 
-    // goal method -> like an function , work for only player class; 
-    goal(){
-        console.log('scored a goal');
-    }
-    
-    cup(){
-        console.log('this player won a cup');
-    }
+  cup() {
+    console.log("this player won a cup");
+  }
 }
 
-// create an object of class ; 
-const player1 = new player('rakib', 32); // when we create object for class player() call the constructor class auto; 
-const player2 = new player('jamal', 29);
+// create an object of class ;
+const player1 = new player("rakib", 32); // when we create object for class player() call the constructor class auto;
+const player2 = new player("jamal", 29);
 
-// player scored a goal ; 
+// player scored a goal ;
 player2.goal();
 
 console.log(player1);
 console.log(player2);
-
