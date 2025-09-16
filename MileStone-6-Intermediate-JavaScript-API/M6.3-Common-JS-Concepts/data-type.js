078359d147054892ecd61e003a18cb536aca760c
Jahid Hasan <jahidjubaer17@gmail.com>
2025-09-16 16:05:58 +0600

    data type

---
+++ MileStone-6-Intermediate-JavaScript-API/M6.3-Common-JS-Concepts/data-type.js
@@ -0,0 +1,16 @@
+// mainly 2 data type in js; 
+/*
+1. primitive -> string , number, boolean; 
+2. non primitive -> object, array; 
+*/ 
+
+// js is dynamic programming language ; 
+//like  it set the variable type after set the value of variable ; 
+let hello = 343; 
+hello = "jahid hasan";
+hello = false;
+hello = undefined; 
+hello = null; 
+hello = [3,2,4];
+hello = {name: 'rakib', age: 23};
+console.log(typeof hello); // here i assign the diff type of value , change the type all the time; 
