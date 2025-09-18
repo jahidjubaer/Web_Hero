b6c394a5e0215afaab2639bd3ba9cde9b9829f3a
Jahid Hasan <jahidjubaer17@gmail.com>
2025-09-18 23:57:02 +0600

    design added to the card

--- MileStone-6-Intermediate-JavaScript-API/M6.4-API-JSON-Data-Load/post.js
+++ MileStone-6-Intermediate-JavaScript-API/M6.4-API-JSON-Data-Load/post.js
@@ -1,23 +1,38 @@
 // first fetch the data form url ;
 // before get into that write the function for onclick; we could call form here. but lets do with function ;
 
-// its explain in fetch.js 
+// its explain in fetch.js
 const showPost = () => {
   const url = "https://jsonplaceholder.typicode.com/posts";
   fetch(url)
     .then((response) => response.json())
     .then((posts) => printData(posts));
-
-// post arguments pass with array object ; 
-  const printData = (posts) => {
-    // for getting each object from the array looping ; 
-    posts.forEach((element) => {
-    // creating a new elements li and change the inner text and then append as a child to its parent class ; 
-      const li = document.createElement("li");
-      li.innerText = element.title;
-
-      const ul = document.getElementById("post-title-list");
-      ul.appendChild(li);
-    });
-  };
 };
+
+// post arguments pass with array object ;
+const printData = (posts) => {
+  const containerDiv = document.getElementById("post-container");
+  // to prevent repeating ;
+  containerDiv.innerHTML = " ";
+
+  // for getting each object from the array looping ;
+  posts.forEach((element) => {
+    // creating a new elements li and change the inner text and then append as a child to its parent class ;
+    const div = document.createElement("div");
+
+    // here i change the inner html ; with dynamic way ;
+    div.innerHTML = `
+      <div class="post-card">
+        <h1>${element.title}</h1>
+        <p>
+          ${element.body}
+        </p>
+      </div>
+      `;
+
+    containerDiv.appendChild(div);
+  });
+};
+
+// here i call that it will show without click ;
+showPost();
