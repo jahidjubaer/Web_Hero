903d361fc97e0e1517400e32c90821fca65e7ca1
Jahid Hasan <jahidjubaer17@gmail.com>
2025-09-22 21:46:09 +0600

    time-interval

--- MileStone-6-Intermediate-JavaScript-API/M6.5-More-JS/time-interval.js
+++ MileStone-6-Intermediate-JavaScript-API/M6.5-More-JS/time-interval.js
@@ -1,1 +1,18 @@
+function getData() {
+  console.log("one");
+  console.log("two");
 
+  let num = 0;
+  const intervalId = setInterval(() => {
+    console.log(num++);
+
+    if (num == 10) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+
+  console.log("four");
+  console.log("five");
+}
+
+getData();
