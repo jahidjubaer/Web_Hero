688b8991616014270e5b88d7959cf5faa283ebfe
Jahid Hasan <jahidjubaer17@gmail.com>
2025-09-22 23:34:03 +0600

    objcet set and get in local storage

--- MileStone-6-Intermediate-JavaScript-API/M6.5.1-Local-Storage/local-storage.js
+++ MileStone-6-Intermediate-JavaScript-API/M6.5.1-Local-Storage/local-storage.js
@@ -11,3 +11,26 @@ const getDataLS = () => {
   const num = localStorage.getItem("number");
   console.log("get number", num);
 };
+
+// for non-primitive data we can't store directly ; need to sum modification ;
+const setObjectLS = () => {
+  const consumer = { name: "abbas", bill: 1200, payBill: 1300 };
+  // we can't set the consumer directly in local storage , it wil consider as string ;
+  // so we need to stringify the object ;
+
+  const consumerJson = JSON.stringify(consumer);
+
+  // now we can set he stringify value easily ;
+  localStorage.setItem("consumer", consumerJson);
+};
+
+// how can we get the object or non primitive value form local storage and access the value of object ;
+
+const getObjectLS = () => {
+  const consumerJson = localStorage.getItem("consumer");
+  // so we got the value as string . i mean stringify value ;
+
+  // now parse the value of object;
+  const consumer = JSON.parse(consumerJson);
+  console.log(consumer.name);
+};
