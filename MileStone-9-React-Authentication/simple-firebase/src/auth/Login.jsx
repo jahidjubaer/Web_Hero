7c0da1bdc575737e3f35d1cc8e56ab1061194464
Jahid Hasan <jahidjubaer17@gmail.com>
2025-10-21 09:57:27 +0600

    firebase auth

--- MileStone-9-React-Authentication/simple-firebase/src/auth/Login.jsx
+++ MileStone-9-React-Authentication/simple-firebase/src/auth/Login.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../firebase/firebase.init";
+
+const provider = new GoogleAuthProvider();
 
 const Login = () => {
-    return (
+  // user state
+  const [user, setUser] = useState(null);
+  // login with popup google
+  const handleLoginBtn = () =>
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  //   handleSignout
+  const handleSignout = () => {
+    signOut(auth)
+      .then(() => {
+        console.log(user);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Login form here</h2>
+
+      {/* button  */}
+      {user ? (
+        <button onClick={handleSignout}>Signout</button>
+      ) : (
+        <button onClick={handleLoginBtn}>Login with google</button>
+      )}
+
+      {/* if there have any user data i mean if anyone login then ;  */}
+      {user && (
         <div>
-           <h2>Login form here</h2> 
+          <h1>{user.displayName}</h1>
+          <h3>{user.email}</h3>
+          <img src={user.photoURL} alt="" />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Login;
--- MileStone-9-React-Authentication/simple-firebase/src/firebase/firebase.init.js
+++ MileStone-9-React-Authentication/simple-firebase/src/firebase/firebase.init.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,9 +11,12 @@ const firebaseConfig = {
   projectId: "simple-firebase-auth-95fbc",
   storageBucket: "simple-firebase-auth-95fbc.firebasestorage.app",
   messagingSenderId: "844421192915",
-  appId: "1:844421192915:web:8269f3909c9870cf004826"
+  appId: "1:844421192915:web:8269f3909c9870cf004826",
 };
 
 // Initialize Firebase
 // eslint-disable-next-line no-unused-vars
 const app = initializeApp(firebaseConfig);
+
+// export for further use ;
+export const auth = getAuth(app);
