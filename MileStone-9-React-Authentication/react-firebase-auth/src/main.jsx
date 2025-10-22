7e6f007e1dc90ce209c7683a6eff9c8fa8067d73
Jahid Hasan <jahidjubaer17@gmail.com>
2025-10-22 23:53:53 +0600

    project and router setup

---
+++ MileStone-9-React-Authentication/react-firebase-auth/src/main.jsx
@@ -0,0 +1,37 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from "./Layout/Root.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Register from "./pages/register/Register.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "/register",
+        Component: Register,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
